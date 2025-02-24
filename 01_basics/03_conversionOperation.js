23cf7e9f2e7be4a62b17d2fb38a20c3871b44708
chawla-ji <104126012+chawla-ji@users.noreply.github.com>
2025-02-24 09:25:47 +0000

    Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,54 @@
+// // let score = "33"
+// // let score = "33abc"
+// let score = null
+// // let score = undefined
+// // let score = "harsh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// // let isLoggedIn = -3
+// // let isLoggedIn = ""
+// let isLoggedIn = "abcd"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// // 0 => false
+// // non 0 => true
+// // "" => false
+// // "non-empty" => true
+
+
+// *********************Operations**************************
+
+let val = 3
+let negVal = -val
+console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " harsh"
+
+let str3 = str1 + str2
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 1 + "2");
+console.log("1" + 1 + 2);
+
+
+n1 = n2 = n3 = 2+2
